ab17f2abf6faac29e850da08227b9cd49b75b101
eilyri <khaeun1029@gmail.com>
2025-02-08 00:59:08 +0900

    feat: 텍스트뷰 구성

--- TypingApp/TypingApp/Sources/Views/Typing/TypingViewController.swift
+++ TypingApp/TypingApp/Sources/Views/Typing/TypingViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class TypingViewController: BaseViewController {
-    let speedView = TypingSpeedView()
+    private let speedView = TypingSpeedView()
+    
+    private let placeholderTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textColor = UIColor.gray300
+        textView.backgroundColor = UIColor.gray200
+        textView.font = UIFont.pretendard(type: .regular, size: 20)
+        textView.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 10, right: 16)
+        
+        textView.isEditable = false
+        
+        textView.text = "어른이 되는 것이 끔찍한 이유는 아무도 우리에게 관심이 없고, 앞으로는 스스로 모든 일을 처리하고 세상이 어떤 식으로 돌아가는지 파악해야 한다는 것을 깨닫는 순간이 찾아오기 때문이다."
+        
+        return textView
+    }()
+    
+    private let typingTextView: UITextView = {
+        let textView = UITextView()
+        textView.textColor = UIColor.gray300
+        textView.backgroundColor = .clear
+        textView.font = UIFont.pretendard(type: .regular, size: 20)
+        textView.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 10, right: 16)
+        
+        textView.becomeFirstResponder()
+        
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
         setSpeedView()
+        setTextView()
     }
 
     private func setNavigationBar() {
@@ -23,5 +51,15 @@ class TypingViewController: BaseViewController {
     
     private func setSpeedView() {
         view.addSubview(speedView, autoLayout: [.topNext(to: navigationBar, constant: 0), .leading(0), .trailing(0), .height(30)])
+    }
+    
+    private func setTextView() {
+        view.addSubview(placeholderTextView, autoLayout: [.topNext(to: speedView, constant: 0), .leading(0), .trailing(0), .bottom(0)])
+        view.addSubview(typingTextView, autoLayout: [
+            .topEqual(to: placeholderTextView, constant: 0),
+            .leadingEqual(to: placeholderTextView, constant: 0),
+            .trailingEqual(to: placeholderTextView, constant: 0),
+            .bottomEqual(to: placeholderTextView, constant: 0)
+        ])
     }
 }
